7847c31d6d05304f703278a8d5dd889578860e6c
Viachaslau Kniha <viachaslau.kniha@itechart-group.com>
2019-06-06 14:43:59 +0300

    add IdModel

---
+++ GiphyH/GiphyH/Models/IdModel.cs
@@ -0,0 +1,14 @@
+﻿using GiphyH.BLL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiphyH.Models
+{
+    public class IdModel
+    {
+        [Encrypt]
+        public int Id { get; set; }
+    }
+}
